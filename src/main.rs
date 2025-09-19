42ba968df3514e78d3d51cd0ac14c827948da531
俺不叫化咕龙 <1801943622@qq.com>
2025-09-19 19:32:27 +0800

    Add handler3 to main and update Engine run calls for improved functionality

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,18 @@ fn main() {
         println!("injection engine says {i} and {u}");
     }
 
+    fn handler3(i: &i32, u: &u8, s: &&str, v: &Vec<i32>) {
+        println!("injection engine says {i}, {u}, {s}, and {v:?}");
+    }
+
+    let vec = vec![1, 2, 3];
     injection_engine::EngineBuilder::default()
         .with_state(42i32)
         .with_state(7u8)
+        .with_state("Hello, world!")
+        .with_state(vec)
         .build()
         .run(handler)
-        .run(handler2);
+        .run(handler2)
+        .run(handler3);
 }
